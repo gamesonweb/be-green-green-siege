07b1af7a845e0b0bf902a0a12a0373af46648d45
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2023-03-25 16:12:07 +0100

    import scene

--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,7 @@
 import { GameUtils } from './game-utils';
 import * as BABYLON from 'babylonjs';
 import * as GUI from 'babylonjs-gui';
-import { FreeCamera } from 'babylonjs';
+import { FreeCamera, SceneLoader } from 'babylonjs';
 
 export class Game {
 
@@ -9,6 +9,7 @@ export class Game {
     private _engine: BABYLON.Engine;
     private _scene: BABYLON.Scene;
     private _camera: BABYLON.FreeCamera;
+    private _assetManager: BABYLON.AssetsManager;
     private _light: BABYLON.Light;
 
 
@@ -16,6 +17,7 @@ export class Game {
         // Create canvas and engine
         this._canvas = <HTMLCanvasElement>document.getElementById(canvasElement);
         this._engine = new BABYLON.Engine(this._canvas, true);
+        
     }
 
     /**
@@ -24,8 +26,8 @@ export class Game {
     async createScene(): Promise<void> {
         // create a basic BJS Scene object
         this._scene = new BABYLON.Scene(this._engine);
+        this._assetManager = new BABYLON.AssetsManager(this._scene);
 
-        
 
         // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
         this._camera = new FreeCamera("Camera", new BABYLON.Vector3(0, 5, -10), this._scene);
@@ -35,15 +37,40 @@ export class Game {
         // create the skybox
         let skybox = GameUtils.createSkybox("skybox", "./assets/texture/skybox/TropicalSunnyDay", this._scene);
         // creates the sandy ground
-        let ground = GameUtils.createGround(this._scene);
-        // creates the watermaterial and adds the relevant nodes to the renderlist
+        // let ground = GameUtils.createGround(this._scene);
+        // // creates the watermaterial and adds the relevant nodes to the renderlist
         
-        // let xr = await this._scene.createDefaultXRExperienceAsync({
-        //     floorMeshes: [ground]
-        // });
+        let meshTask = this._assetManager.addMeshTask("test", "", "./assets/", "test.glb");
+
+        meshTask.onSuccess = (task) => {
+            console.log("Loading mesh");
+            
+            task.loadedMeshes.forEach((mesh) => {
+                console.log(mesh.name);
+            });
+        }
+        this._assetManager.load();
+        
+
+        this._scene.executeWhenReady(async () => {
+            console.log("Scene is ready");
+            
+            let supported = await BABYLON.WebXRSessionManager.IsSessionSupportedAsync('immersive-ar');
+            let platform = this._scene.getMeshByName("Platform");
+            let spawnPoint = this._scene.getMeshByName("SpawnPoint");
+            this._camera.position = spawnPoint.position.clone();
+            
+            if (supported) {
+                console.log('supported');
+                let xr = await this._scene.createDefaultXRExperienceAsync({
+                    floorMeshes: [platform]
+                });
+            } else {
+                console.log('not supported');
+            }
+        });
+        
     }
-
-
 
     /**
      * Starts the animation loop.
