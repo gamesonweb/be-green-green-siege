7de5361a764c2cd04409483e6caea35b8e8318e4
Rémi Cérès <remi.ceres@inria.fr>
2023-04-15 22:24:14 +0200

    Fix instance name in instance Loader

--- src/instanceLoader.ts
+++ src/instanceLoader.ts
@@ -2,13 +2,14 @@ import * as BABYLON from 'babylonjs';
 
 export class InstanceLoader {
     private _scene: BABYLON.Scene;
-
     private _botRef: BABYLON.Mesh;
+    private _instanceCounter: number;
 
     constructor(scene: BABYLON.Scene) {
         this._scene = scene;
         this._botRef = this._scene.getMeshByName('Robot') as BABYLON.Mesh;
         this.hideMeshWithChildren(this._botRef);
+        this._instanceCounter = 0;
     }
 
     private hideMeshWithChildren(mesh: BABYLON.AbstractMesh) {
@@ -20,12 +21,15 @@ export class InstanceLoader {
         });
     }
 
-    private createMeshInstanceWithChildren(mesh, newInstanceName) {
+    private createMeshInstanceWithChildren(mesh, newInstanceName, uniqueSuffix) {
         const newInstance = mesh.createInstance(newInstanceName);
 
-        mesh.getChildren().forEach((child, index) => {
+        // Add a unique identifier to the robot instance
+        newInstance.metadata = { instanceId: uniqueSuffix };
+
+        mesh.getChildren().forEach((child) => {
             if (child instanceof BABYLON.AbstractMesh) {
-                const childInstance = this.createMeshInstanceWithChildren(child, `${newInstanceName}_child_${index}`);
+                const childInstance = this.createMeshInstanceWithChildren(child, `instance_${child.name}_${uniqueSuffix}`, uniqueSuffix);
                 childInstance.parent = newInstance;
             }
         });
@@ -34,7 +38,36 @@ export class InstanceLoader {
     }
 
     public getBot(newInstanceName: string) {
-        const bot = this.createMeshInstanceWithChildren(this._botRef, newInstanceName);
+        // Generate a unique identifier based on the instance counter
+        const uniqueSuffix = this._instanceCounter++;
+        const bot = this.createMeshInstanceWithChildren(this._botRef, newInstanceName, uniqueSuffix);
         return bot;
+    }
+
+    public findInstanceSubMeshByName(instanceMesh: BABYLON.AbstractMesh, subMeshName: string): BABYLON.AbstractMesh {
+        if (instanceMesh.metadata && instanceMesh.metadata.instanceId !== undefined) {
+            const instanceId = instanceMesh.metadata.instanceId;
+
+            return this.recursiveFindInstanceSubMeshByName(instanceMesh, instanceId, subMeshName);
+        }
+
+        return null;
+    }
+
+    private recursiveFindInstanceSubMeshByName(mesh: BABYLON.AbstractMesh, instanceId: number, subMeshName: string) {
+        if (mesh.metadata && mesh.metadata.instanceId === instanceId && mesh.name === `instance_${subMeshName}_${instanceId}`) {
+            return mesh;
+        }
+
+        for (let child of mesh.getChildren()) {
+            if (child instanceof BABYLON.AbstractMesh) {
+                const result = this.recursiveFindInstanceSubMeshByName(child, instanceId, subMeshName);
+                if (result) {
+                    return result;
+                }
+            }
+        }
+
+        return null;
     }
 }
