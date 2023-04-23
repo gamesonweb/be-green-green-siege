e1bdb1934de6a477267dca914a22efbced75ddf1
Rémi Cérès <remi.ceres@inria.fr>
2023-04-23 19:43:53 +0200

    the animations respect the speed of time

--- src/AnimationController.ts
+++ src/AnimationController.ts
@@ -1,20 +1,24 @@
 import * as BABYLON from 'babylonjs';
 
 class AnimationController {
-
-    BarelShot: BABYLON.AnimationGroup;
-    GunShot: BABYLON.AnimationGroup;
-    overHeatBack: BABYLON.AnimationGroup;
-    overHeatFront: BABYLON.AnimationGroup;
-
+    public BarelShot: BABYLON.AnimationGroup;
+    public overHeatBack: BABYLON.AnimationGroup;
+    public overHeatFront: BABYLON.AnimationGroup;
+    public gunIdle: BABYLON.AnimationGroup;
 
     public playAnimation(animation: BABYLON.AnimationGroup, loop: boolean = false, speedRatio: number = 1): void {
         animation.reset();
         animation.start(loop, speedRatio);
     }
-    
+
+    public setSpeedRatio(speedRatio: number): void {
+        this.BarelShot.speedRatio = speedRatio;
+        this.overHeatBack.speedRatio = speedRatio;
+        this.overHeatFront.speedRatio = speedRatio;
+        this.gunIdle.speedRatio = speedRatio;
+    }
 }
 
 const animations = new AnimationController();
 
-export {animations}
+export { animations };
--- src/TimeControl.ts
+++ src/TimeControl.ts
@@ -1,8 +1,11 @@
+import { animations } from './AnimationController';
+
 export class TimeControl {
     private static _timeScale: number = 1;
 
     public static setTimeScale(timeScale: number) {
         this._timeScale = timeScale;
+        animations.setSpeedRatio(timeScale);
     }
 
     public static getTimeScale(): number {
