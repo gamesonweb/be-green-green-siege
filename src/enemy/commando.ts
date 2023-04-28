c72228f05a663a0c20b99debe7f9ce35558fdec9
Rémi Cérès <remi.ceres@inria.fr>
2023-04-28 14:56:20 +0200

    Add random on spawnPoint

--- src/enemy/commando.ts
+++ src/enemy/commando.ts
@@ -1,9 +1,8 @@
 import * as BABYLON from 'babylonjs';
-import { Zone } from './zone';
 import { Enemy } from './Enemy';
+import { Zone } from './zone';
 
 export class Commando {
-
     private _enemies: Enemy[];
     private _destination: BABYLON.Vector3;
     private _zone: Zone;
@@ -12,8 +11,14 @@ export class Commando {
         this._enemies = [];
         this._zone = zone;
         this._destination = this._zone.getRandomPoint();
-        for(let i=0; i<nb; i++) {
-            let enemy = new Enemy(scene, spawnPoint, caracteristics);
+        for (let i = 0; i < nb; i++) {
+            // add random on spawnPoint position to avoid all enemies to be at the same position (create bug on pathfinding)
+            const spanwPointRandom = new BABYLON.Vector3(
+                spawnPoint.x + BABYLON.Scalar.RandomRange(-1, 1),
+                spawnPoint.y + BABYLON.Scalar.RandomRange(-1, 1),
+                spawnPoint.z + BABYLON.Scalar.RandomRange(-1, 1)
+            );
+            let enemy = new Enemy(scene, spanwPointRandom, caracteristics);
             enemy.setDestination(this._destination);
             this._enemies.push(enemy);
         }
