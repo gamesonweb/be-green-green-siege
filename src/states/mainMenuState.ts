5e67eb3a341ee09afd80817de0c78a8ee9cfe681
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2023-04-28 17:56:53 +0200

    New UI

--- src/states/mainMenuState.ts
+++ src/states/mainMenuState.ts
@@ -1,20 +1,18 @@
 import * as BABYLON from 'babylonjs';
 import { State } from './state';
 import { StateManager, StatesEnum } from './stateManager';
+import  MainGUI  from '../ui/mainUI';
 
 export class MainMenuState implements State {
     private _scene: BABYLON.Scene;
     private _stateManager: StateManager;
-    private _light: BABYLON.HemisphericLight;
 
-    private _levelTestBotSelector: BABYLON.Mesh;
-    private _levelTestGunSelector: BABYLON.Mesh;
-    private _levelEmptySelector: BABYLON.Mesh;
-    private _level: BABYLON.Mesh;
+    private _mainUI: MainGUI; 
 
     constructor(scene: BABYLON.Scene, stateManager: StateManager) {
         this._scene = scene;
         this._stateManager = stateManager;
+        this._mainUI = new MainGUI(this._scene, this._scene.activeCamera, this._stateManager);
     }
     shieldSize: number;
 
@@ -25,39 +23,11 @@ export class MainMenuState implements State {
     }
 
     public load(): void {
-        // create a basic light, aiming 0,1,0 - meaning, to the sky
-        // this._light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 200, 0), this._scene);
-        // this._light.intensity = 0.3;
-        this._levelTestBotSelector = this.createLevel(new BABYLON.Vector3(15, 2, -25), StatesEnum.LEVELTESTBOT);
-        this._levelTestGunSelector = this.createLevel(new BABYLON.Vector3(17, 2, -25), StatesEnum.LEVELTESTGUN);
-        this._levelEmptySelector = this.createLevel(new BABYLON.Vector3(19, 2, -25), StatesEnum.EMPTY);
-        this._level = this.createLevel(new BABYLON.Vector3(21, 2, -25), StatesEnum.LEVEL, 1);
+        this._mainUI.load();
     }
 
     public dispose(): void {
-        // dispose the light
-        // this._light.dispose();
-
-        // dispose the cube
-        this._levelTestBotSelector.dispose();
-        this._levelTestGunSelector.dispose();
-        this._levelEmptySelector.dispose();
-        this._level.dispose();
-    }
-
-    private createLevel(position: BABYLON.Vector3, levelenum: StatesEnum, levelNumber: number = undefined): BABYLON.Mesh {
-        const box = BABYLON.MeshBuilder.CreateBox('box', {}, this._scene);
-        box.position = position;
-
-        // if click on the cube, switch to level test boat
-        box.actionManager = new BABYLON.ActionManager(this._scene);
-        box.actionManager.registerAction(
-            new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, () => {
-                this._stateManager.switchState(levelenum, levelNumber);
-            })
-        );
-
-        return box;
+        this._mainUI.dispose();
     }
 
     public animate(deltaTime: number): void {}
---
+++ src/ui/mainUI.ts
@@ -0,0 +1,82 @@
+import * as BABYLON from 'babylonjs';
+import * as GUI from 'babylonjs-gui';
+import { Game } from '../game';
+import UI from './ui';
+import { StatesEnum } from '../states/stateManager';
+import { StateManager } from '../states/stateManager';
+
+export default class MainGUI implements UI{
+
+    private _scene: BABYLON.Scene;
+    private _camera: BABYLON.Camera;
+
+    private _manager: GUI.GUI3DManager;
+    private _stateManager: StateManager;
+    
+    public anchor: BABYLON.TransformNode;
+
+
+    constructor(scene: BABYLON.Scene, camera: BABYLON.Camera, _stateManager: StateManager) {
+        this._scene = scene;
+        this._camera = camera;
+        this._manager = new GUI.GUI3DManager(this._scene);
+        this._stateManager = _stateManager;
+    }
+
+    private createLevelButton(text: string, levelenum: StatesEnum, levelNumber: number = undefined) {
+        let button = new GUI.HolographicButton("button");
+        button.text = text;
+        button.onPointerClickObservable.add(() => {
+            this._stateManager.switchState(levelenum, levelNumber);
+        });
+
+        return button;
+    }
+
+    load() {
+        // this._scene.debugLayer.show();
+
+        this.anchor = new BABYLON.TransformNode("anchorPanel");
+        this.anchor.rotate(BABYLON.Axis.Y, Math.PI, BABYLON.Space.LOCAL);
+        
+        let panel = new GUI.StackPanel3D();
+    
+        this._manager.addControl(panel);
+        panel.linkToTransformNode(this.anchor);
+        this.anchor.position = this._camera.position.clone();
+        this.anchor.position.z -= 5;
+
+        let leftPanel = new GUI.StackPanel3D(true)
+        leftPanel.isVertical = true;
+        panel.addControl(leftPanel);
+        leftPanel.position.x = -1.5; // Position to the left of center
+        
+        let rightPanel = new GUI.StackPanel3D(true)
+        rightPanel.isVertical = true;
+        panel.addControl(rightPanel);
+        rightPanel.position.x = 1.5; // Position to the right of center
+        
+        let middlePanel = new GUI.StackPanel3D(true);
+        middlePanel.isVertical = true;
+        panel.addControl(middlePanel);
+        middlePanel.position.x = 0; // Position in the center
+        
+    
+        // Add controls to left panel
+        let button1 = this.createLevelButton("Level 1", StatesEnum.LEVEL, 1);
+        leftPanel.addControl(button1);
+
+        // Add controls to right panel
+        let button3 = this.createLevelButton("Test Bot", StatesEnum.LEVELTESTBOT);
+        rightPanel.addControl(button3);
+
+        // Add controls to middle panel
+        let button5 = this.createLevelButton("Test Gun", StatesEnum.LEVELTESTGUN);
+        middlePanel.addControl(button5);
+    }
+
+    dispose() {
+        this._manager.dispose();
+        this.anchor.dispose();
+    }
+}
